92d342e253817443241b943fcd20e58ac2798e7c
Abdulla Aldhawadi <abdullaaldhawadi@D103-18.local>
2026-04-26 11:22:26 +0300

    Initial commit

---
+++ asd/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  asd
+//
+//  Created by Abdulla Aldhawadi on 22/02/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ asd/asdApp.swift
@@ -0,0 +1,17 @@
+//
+//  asdApp.swift
+//  asd
+//
+//  Created by Abdulla Aldhawadi on 22/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct asdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
